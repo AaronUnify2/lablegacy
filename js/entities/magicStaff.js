0b1d9e62b0d31b4124d86e4e4140896bb64451e7
AaronUnify2 <unify.goalsetter@gmail.com>
2025-04-11 13:41:16 +1000

    Update magicStaff.js

--- js/entities/magicStaff.js
+++ js/entities/magicStaff.js
@@ -1,1 +1,14 @@
-
+// Simplified version to test:
+export class MagicStaff {
+    constructor(scene, camera) {
+        this.scene = scene;
+        this.camera = camera;
+        console.log('MagicStaff constructor called');
+        // Minimal initialization
+    }
+    
+    // Add dummy methods that do nothing
+    ensureLightState() {}
+    toggleLight() {}
+    // etc.
+}
