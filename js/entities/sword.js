448c80ec0b9b414bd00bfd692195dfd84612e5c2
AaronUnify2 <unify.goalsetter@gmail.com>
2025-04-15 07:17:37 +1000

    Update sword.js

--- js/entities/sword.js
+++ js/entities/sword.js
@@ -8,7 +8,7 @@ export class Sword {
         this.handleLength = 0.2; // Length of the handle
         
         // Sword position offset relative to camera - adjusted for right hand dual wielding
-        this.positionOffset = new THREE.Vector3(0.35, -0.35, -1);
+        this.positionOffset = new THREE.Vector3(0.35, -0.6, -1);
         
         // Sword animation variables
         this.bobAmount = 0.02;
